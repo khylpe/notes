22152ff0979bee277e086de6760117b520cd0d99
Arthur CRAHÉ <crahe.arthur@gmail.com>
2024-08-08 15:38:39 +0200

    Create custom-modules.d.ts

---
+++ src/custom-modules.d.ts
@@ -0,0 +1,14 @@
+declare module 'markdown-it-katex' {
+       const markdownItKatex: any;
+       export default markdownItKatex;
+}
+
+declare module 'markdown-it-deflist' {
+       const markdownItDeflist: any;
+       export default markdownItDeflist;
+}
+
+declare module 'markdown-it-mark' {
+       const markdownItMark: any;
+       export default markdownItMark;
+}   
