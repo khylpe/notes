1062441f3cf2086cc6be6a615fc093e7422dcab0
Arthur CRAHÉ <crahe.arthur@gmail.com>
2024-09-18 20:20:19 +0200

    responsive & remove unused tabs in profile page

--- client/src/views/ProfilView.vue
+++ client/src/views/ProfilView.vue
@@ -1,62 +1,66 @@
 <template>
-       <div class="mt-8 mx-4">
-              <a-tabs v-model:activeKey="activeKey" size="large" tabPosition="right">
-                     <a-tab-pane key="information" tab="Main information">
-                            <span class="text-3xl">Information</span>
-                            <div class="flex flex-col md:flex-row md:items-center mt-6">
-                                   <!-- maybe remove items-center if too much content-->
-                                   <div class="flex flex-row mt-3">
-                                          <a-avatar v-if="profilPicture" class="ml-3"
-                                                 style="width: 100px; height: 100px;" :src="profilPicture" />
-                                          <a-avatar v-else :size="100">
-                                                 <template #icon>
-                                                        <user-outlined />
-                                                 </template>
-                                          </a-avatar>
-                                   </div>
-                                   <div class="flex flex-col md:ml-14">
-                                          <div class="flex flex-col sm:flex-row sm:items-center mt-3">
-                                                 <span>Username</span>
-                                                 <div class="w-fit">
-                                                        <a-input class="sm:ml-3 w-full" v-model:value="username"
-                                                               placeholder="Your" />
-                                                 </div>
+       <div class="mt-8 mx-4 text-white">
+              <div key="information" tab="Main information" :class="isMobile ? 'flex flex-col items-center' : ''">
+                     <span class="text-3xl">Information</span>
+                     <div class="flex flex-col md:flex-row md:items-center mt-6">
+                            <!-- maybe remove items-center if too much content-->
+                            <div :class="isMobile ? 'flex flex-col items-center' : 'flex flex-row'">
+                                   <a-avatar v-if="profilPicture" class="ml-3" style="width: 100px; height: 100px;"
+                                          :src="profilPicture" />
+                                   <a-avatar v-else :size="100">
+                                          <template #icon>
+                                                 <user-outlined />
+                                          </template>
+                                   </a-avatar>
+                            </div>
+                            <div class="flex flex-col md:ml-14">
+                                   <div class="flex flex-col sm:flex-row sm:items-center mt-3">
+                                          <span>Username</span>
+                                          <div class="w-fit">
+                                                 <a-input class="sm:ml-3 w-full" v-model:value="username"
+                                                        placeholder="Your" />
+                                          </div>
 
-                                          </div>
-                                          <div v-if="email" class="flex flex-col sm:flex-row sm:items-center mt-3">
-                                                 <span>Email</span>
-                                                 <a-input class="sm:ml-3" style="width: fit-content;" disabled
-                                                        v-model:value="email" placeholder="Your email" />
-                                          </div>
+                                   </div>
+                                   <div v-if="email" class="flex flex-col sm:flex-row sm:items-center mt-3">
+                                          <span>Email</span>
+                                          <a-input class="sm:ml-3" style="width: fit-content;" disabled
+                                                 v-model:value="email" placeholder="Your email" />
                                    </div>
                             </div>
-                            <a-button class="mt-20" :disabled="!isProfileModified" @click="onSaveProfile"
-                                   type="primary">Save
-                                   profil</a-button>
-                     </a-tab-pane>
-                     <a-tab-pane key="preferences" disabled tab="Preferences">
-                            Preferences
-                     </a-tab-pane>
-                     <a-tab-pane key="account" disabled tab="Account">
-                            Account
-                     </a-tab-pane>
-              </a-tabs>
+                     </div>
+                     <a-button class="mt-20" :disabled="!isProfileModified" @click="onSaveProfile" type="primary">Save
+                            profil</a-button>
+              </div>
        </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch, onMounted } from 'vue';
+import { ref, computed, watch, onMounted, onUnmounted } from 'vue';
 import { useUserInformationStore } from '@/stores/userInformationStore';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
 import { message } from 'ant-design-vue';
 import { auth } from '@/services/FirebaseConfig';
 
 const userInformationStore = useUserInformationStore();
-const activeKey = ref('information');
 
 const username = ref<string | null>(null);
 const profilPicture = ref<string | null>(null);
 const email = ref<string | null>(null);
+const isMobile = ref(window.innerWidth < 768);
+
+const handleResize = () => {
+       // Update the isMobile ref based on the current window width
+       isMobile.value = window.innerWidth < 768;
+};
+
+onMounted(() => {
+       window.addEventListener('resize', handleResize);
+});
+
+onUnmounted(() => {
+       window.removeEventListener('resize', handleResize);
+});
 
 const initialValues = ref<{ username: string | null; email: string | null }>({
        username: null,
